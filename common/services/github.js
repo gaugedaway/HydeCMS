55801e97e1f2762f5fa9e4d5265f04e7e1dd7056
Adam Bac <abac00s@users.noreply.github.com>
2016-03-09 20:51:57 +0000

    Fixed indentation

--- common/services/github.js
+++ common/services/github.js
@@ -1,182 +1,183 @@
 angular.module('Hyde.Common.Services.Github', ['Hyde.Common.Services.OAuth'])
+
   .service('githubProvider', [
     '$http',
     'oauthProvider',
 
     function($http, oauth) {
 
-    var apiUrl = 'https://api.github.com';
-    var authUserUrl = apiUrl + '/user';
-    var repoUrl = (owner, repo) => apiUrl + '/repos/' + owner + '/' + repo;
-    var contentsUrl = (owner, repo, path) => repoUrl(owner, repo) + '/contents/' + path;
+      var apiUrl = 'https://api.github.com';
+      var authUserUrl = apiUrl + '/user';
+      var repoUrl = (owner, repo) => apiUrl + '/repos/' + owner + '/' + repo;
+      var contentsUrl = (owner, repo, path) => repoUrl(owner, repo) + '/contents/' + path;
 
-    function getObject(url, token, callback) {
-      $http({
-        'method': 'GET',
-        'headers': {'Authorization': 'token ' + token},
-        'url': url
-      }).then(function(response) {
-        callback(response.data);
-      }, function(response) {
-        callback(null);
-      });
+      function getObject(url, token, callback) {
+        $http({
+          'method': 'GET',
+          'headers': {'Authorization': 'token ' + token},
+          'url': url
+        }).then(function(response) {
+          callback(response.data);
+        }, function(response) {
+          callback(null);
+        });
+      }
+
+
+      function postObject(url, token, data, callback) {
+        $http({
+          'method': 'POST',
+          'headers': {'Authorization': 'token ' + token},
+          'url': url,
+          'data': JSON.stringify(data)
+        }).then(function(response) {
+          callback(response.data);
+        }, function(response) {
+          callback(null);
+        });
+      }
+
+
+      function putObject(url, token, data, callback) {
+        $http({
+          'method': 'PUT',
+          'headers': {'Authorization': 'token ' + token},
+          'url': url,
+          'data': JSON.stringify(data)
+        }).then(function(response) {
+          callback(response.data);
+        }, function(response) {
+          callback(null);
+        });
+      }
+
+
+      function patchObject(url, token, data, callback) {
+        $http({
+          'method': 'PATCH',
+          'headers': {'Authorization': 'token ' + token},
+          'url': url,
+          'data': JSON.stringify(data)
+        }).then(function(response) {
+          callback(response.data);
+        }, function(response) {
+          callback(null);
+        });
+      }
+
+
+      function deleteObject(url, token, data, callback) {
+        $http({
+          'method': 'DELETE',
+          'headers': {'Authorization': 'token ' + token},
+          'url': url,
+          'data': data
+        }).then(function(response) {
+          callback(response.data);
+        }, function(response) {
+          callback(null);
+        });
+      }
+
+
+      function getFileSha(token, owner, repo, path, callback) {
+        getObject(contentsUrl(owner, repo, path), token, function(response) {
+           if(response && response.type == 'file') callback(response.sha);
+           else callback(null);
+        });
+      }
+
+
+      this.getFile = function(token, owner, repo, path, callback, encoding='utf-8') {
+        getObject(contentsUrl(owner, repo, path), token, function(response) {
+          if(response && response.type == 'file') {
+            if(response.encoding == 'base64' && encoding == 'utf-8') callback(atob(response.content));
+            else if(response.encoding == 'utf-8' && encoding == 'base64') callback(btoa(response.content));
+            else callback(response.content);
+          }
+          else callback(null);
+        });
+      };
+
+
+      this.fileExists = function(token, owner, repo, path, callback) {
+        getObject(contentsUrl(owner, repo, path), token, function(response) {
+           if(response && response.type == 'file') callback(true);
+           else callback(false);
+        });
+      };
+
+
+      this.listDir = function (token, owner, repo, path, callback) {
+        getObject(contentsUrl(owner, repo, path), token, callback);
+      };
+
+
+      this.saveFile = function (token, owner, repo, path, content, message, encoding='utf-8') {
+        if(encoding == 'utf-8') content = btoa(content);
+
+        getFileSha(token, owner, repo, path, function(sha) {
+          // Note: if file doesn't exist, getFileSha returns null.
+          // This way we can check whether the file exeist,
+          // at the same time getting its sha.
+          if(sha) {
+            // File exist - update it.
+            putObject(contentsUrl(owner, repo, path), token, {
+              'message': message,
+              'content': content,
+              'sha': sha
+            }, function(r) {});
+          }
+          else {
+            // File doesn't exist - create one.
+            putObject(contentsUrl(owner, repo, path), token, {
+              'message': message,
+              'content': content
+            }, function(r) {});
+          }
+        });
+      };
+
+
+      this.deleteFile = function(token, owner, repo, path, message) {
+        getFileSha(token, owner, repo, path, function(sha) {
+           if(sha) {
+             deleteObject(contentsUrl(owner, repo, path), token, {
+               'message': message,
+               'sha': sha
+             }, function(r) {});
+           }
+        });
+      };
+
+
+      this.listRepos = function(token, callback) {
+        getObject(apiUrl + '/user/repos', token, callback);
+      };
+
+
+      this.getRepo = function(token, owner, repo, callback) {
+        getObject(repoUrl(owner, repo), token, callback);
+      };
+
+
+      this.createRepo = function(token, options) {
+        postObject(apiUrl + '/user/repos', token, options, function(r) {});
+      };
+
+
+      this.editRepo = function(token, owner, repo, options) {
+        patchObject(repoUrl(owner, repo), token, options, function(r) {});
+      };
+
+
+      this.deleteRepo = function(token, owner, repo) {
+        deleteObject(repoUrl(owner, repo), token, {}, function(r) {});
+      };
+
+
+      this.getUser = function(token, callback) {
+        getObject(authUserUrl, token, callback);
+      };
     }
-
-
-    function postObject(url, token, data, callback) {
-      $http({
-        'method': 'POST',
-        'headers': {'Authorization': 'token ' + token},
-        'url': url,
-        'data': JSON.stringify(data)
-      }).then(function(response) {
-        callback(response.data);
-      }, function(response) {
-        callback(null);
-      });
-    }
-
-
-    function putObject(url, token, data, callback) {
-      $http({
-        'method': 'PUT',
-        'headers': {'Authorization': 'token ' + token},
-        'url': url,
-        'data': JSON.stringify(data)
-      }).then(function(response) {
-        callback(response.data);
-      }, function(response) {
-        callback(null);
-      });
-    }
-
-
-    function patchObject(url, token, data, callback) {
-      $http({
-        'method': 'PATCH',
-        'headers': {'Authorization': 'token ' + token},
-        'url': url,
-        'data': JSON.stringify(data)
-      }).then(function(response) {
-        callback(response.data);
-      }, function(response) {
-        callback(null);
-      });
-    }
-
-
-    function deleteObject(url, token, data, callback) {
-      $http({
-        'method': 'DELETE',
-        'headers': {'Authorization': 'token ' + token},
-        'url': url,
-        'data': data
-      }).then(function(response) {
-        callback(response.data);
-      }, function(response) {
-        callback(null);
-      });
-    }
-
-
-    function getFileSha(token, owner, repo, path, callback) {
-      getObject(contentsUrl(owner, repo, path), token, function(response) {
-         if(response && response.type == 'file') callback(response.sha);
-         else callback(null);
-      });
-    };
-
-
-    this.getFile = function(token, owner, repo, path, callback, encoding='utf-8') {
-      getObject(contentsUrl(owner, repo, path), token, function(response) {
-        if(response && response.type == 'file') {
-          if(response.encoding == 'base64' && encoding == 'utf-8') callback(atob(response.content));
-          else if(response.encoding == 'utf-8' && encoding == 'base64') callback(btoa(response.content));
-          else callback(response.content);
-        }
-        else callback(null);
-      });
-    };
-
-
-    this.fileExists = function(token, owner, repo, path, callback) {
-      getObject(contentsUrl(owner, repo, path), token, function(response) {
-         if(response && response.type == 'file') callback(true);
-         else callback(false);
-      });
-    };
-
-
-    this.listDir = function (token, owner, repo, path, callback) {
-      getObject(contentsUrl(owner, repo, path), token, callback);
-    };
-
-
-    this.saveFile = function (token, owner, repo, path, content, message, encoding='utf-8') {
-      if(encoding == 'utf-8') content = btoa(content);
-
-      getFileSha(token, owner, repo, path, function(sha) {
-        // Note: if file doesn't exist, getFileSha returns null.
-        // This way we can check whether the file exeist,
-        // at the same time getting its sha.
-        if(sha) {
-          // File exist - update it.
-          putObject(contentsUrl(owner, repo, path), token, {
-            'message': message,
-            'content': content,
-            'sha': sha
-          }, function(r) {});
-        }
-        else {
-          // File doesn't exist - create one.
-          putObject(contentsUrl(owner, repo, path), token, {
-            'message': message,
-            'content': content
-          }, function(r) {});
-        }
-      });
-    };
-
-
-    this.deleteFile = function(token, owner, repo, path, message) {
-      getFileSha(token, owner, repo, path, function(sha) {
-         if(sha) {
-           deleteObject(contentsUrl(owner, repo, path), token, {
-             'message': message,
-             'sha': sha
-           }, function(r) {});
-         }
-      });
-    };
-
-
-    this.listRepos = function(token, callback) {
-      getObject(apiUrl + '/user/repos', token, callback);
-    };
-
-
-    this.getRepo = function(token, owner, repo, callback) {
-      getObject(repoUrl(owner, repo), token, callback);
-    };
-
-
-    this.createRepo = function(token, options) {
-      postObject(apiUrl + '/user/repos', token, options, function(r) {});
-    };
-
-
-    this.editRepo = function(token, owner, repo, options) {
-      patchObject(repoUrl(owner, repo), token, options, function(r) {});
-    };
-
-
-    this.deleteRepo = function(token, owner, repo) {
-      deleteObject(repoUrl(owner, repo), token, {}, function(r) {});
-    };
-
-
-    this.getUser = function(token, callback) {
-      getObject(authUserUrl, token, callback);
-    };
-
-  }]);
+  ]);
--- common/services/oauth.js
+++ common/services/oauth.js
@@ -5,6 +5,7 @@
 */
 
 angular.module('Hyde.Common.Services.OAuth', ['Hyde.Core.Config'])
+
   .service('oauthProvider', [
     '$http',
     '$location',
@@ -17,52 +18,53 @@ angular.module('Hyde.Common.Services.OAuth', ['Hyde.Core.Config'])
 
     function($http, $location, $window, $rootScope, githubUrl, clientId, gatekeeperUrl, scope) {
 
-    // Function used to redirect in case the code written in url
-    // doesn't work, i.e. when user reloaded the page;
-    // for example if the current url is
-    // http://myapp.com/?code=fh438bnvcueb73#/login?abc=def,
-    // redirect() will redirect it to
-    // http://myapp.com/#/login?abc=def.
-    function redirect() {
-      $window.location.href = $location.protocol() + '://' + $location.host() + '/#' + $location.url();
-    }
-
-
-    function getTokenHelper(callback) {
-      // Check if the code already is in url.
-      var code = window.location.href.match(/\?code=(.*)/);
-
-      // If not, redirect to github authentication page.
-      if(!code){
-        $window.location.href = githubUrl + '?client_id=' + clientId + '&redirect_uri=' + window  .encodeURIComponent($location.absUrl()) + '&scope=' + scope.join(',');
+      // Function used to redirect in case the code written in url
+      // doesn't work, i.e. when user reloaded the page;
+      // for example if the current url is
+      // http://myapp.com/?code=fh438bnvcueb73#/login?abc=def,
+      // redirect() will redirect it to
+      // http://myapp.com/#/login?abc=def.
+      function redirect() {
+        $window.location.href = $location.protocol() + '://' + $location.host() + '/#' + $location.url();
       }
 
 
-      // If yes, get the code...
-      if(code) {
-        // The transformatin below is necessary, because GitHub API places
-        // the code before # character, for example, if return url would be
-        // http://abc.com/#/login, GitHub would redirect to somenthing like
-        // http://abc.com/?code=ajffjuhgffjhfjaj#login, so we need to separate
-        // the code from the path after #.
-        code = code[1].split('#')[0];
-        $http.get(gatekeeperUrl + code).then(function(response) {
-          if(response.data && response.data.hasOwnProperty('token')) callback(response.data.token);
-          // If the response is incorrect, try to redirect.
-          else redirect();
-        }, function(response) {
-          // If there was an error, try to redirect.
-          redirect();
+      function getTokenHelper(callback) {
+        // Check if the code already is in url.
+        var code = window.location.href.match(/\?code=(.*)/);
+
+        // If not, redirect to github authentication page.
+        if(!code){
+          $window.location.href = githubUrl + '?client_id=' + clientId + '&redirect_uri=' + window  .encodeURIComponent($location.absUrl()) +   '&scope=' + scope.join(',');
+        }
+
+
+        // If yes, get the code...
+        if(code) {
+          // The transformatin below is necessary, because GitHub API places
+          // the code before # character, for example, if return url would be
+          // http://abc.com/#/login, GitHub would redirect to somenthing like
+          // http://abc.com/?code=ajffjuhgffjhfjaj#login, so we need to separate
+          // the code from the path after #.
+          code = code[1].split('#')[0];
+          $http.get(gatekeeperUrl + code).then(function(response) {
+            if(response.data && response.data.hasOwnProperty('token')) callback(response.data.token);
+            // If the response is incorrect, try to redirect.
+            else redirect();
+          }, function(response) {
+            // If there was an error, try to redirect.
+            redirect();
+          });
+        }
+      }
+
+
+      this.getToken = function(callback) {
+        if($rootScope.oauthToken) callback($rootScope.oauthToken);
+        else getTokenHelper(function(token) {
+          $rootScope.oauthToken = token;
+          callback($rootScope.oauthToken);
         });
-      }
+      };
     }
-
-
-    this.getToken = function(callback) {
-      if($rootScope.oauthToken) callback($rootScope.oauthToken);
-      else getTokenHelper(function(token) {
-        $rootScope.oauthToken = token;
-        callback($rootScope.oauthToken);
-      });
-    };
-  }]);
+  ]);
