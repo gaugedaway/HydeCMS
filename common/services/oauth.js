ed9f7d7cff8929b0de064a89519f2138e02848af
Adam Bac <abac00s@users.noreply.github.com>
2016-03-09 15:03:00 +0000

    Reorganised project

--- common/services/oauth.js
+++ common/services/oauth.js
@@ -5,7 +5,7 @@
 */
 
 angular.module('OAuth', [])
-  .service('oauthProvider', ['$http', '$location', '$window', function($http, $location, $window) {
+  .service('oauthProvider', ['$http', '$location', '$window', '$rootScope', function($http, $location, $window, $rootScope) {
 
   // Configuration; some valued are ereased for security,
   // if you want to experiment wit this code you must configure your
@@ -26,7 +26,17 @@ angular.module('OAuth', [])
   // list of scopes
   var scopes = ['public_repo'];
 
-  this.getToken = function(callback) {
+  // Function used to redirect in case the code written in url
+  // doesn't work, i.e. when user reloaded the page;
+  // for example if the current url is
+  // http://myapp.com/?code=fh438bnvcueb73#/login?abc=def,
+  // redirect() will redirect it to
+  // http://myapp.com/#/login?abc=def.
+  function redirect() {
+    $window.location.href = $location.protocol() + '://' + $location.host() + '/#' + $location.url();
+  }
+
+  function getTokenHelper(callback) {
     // Check if the code already is in url.
     var code = window.location.href.match(/\?code=(.*)/);
 
@@ -35,7 +45,7 @@ angular.module('OAuth', [])
       $window.location.href = github_url + '?client_id=' + client_id + '&redirect_uri=' + window.encodeURIComponent($location.absUrl()) + '&scope=' + scopes.join(',');
     }
 
-    // Now get the token...
+    // If yes, get the code...
     if(code) {
       // The transformatin below is necessary, because GitHub API places
       // the code before # character, for example, if return url would be
@@ -45,10 +55,20 @@ angular.module('OAuth', [])
       code = code[1].split('#')[0];
       $http.get(gate_url + code).then(function(response) {
         if(response.data && response.data.hasOwnProperty('token')) callback(response.data.token);
-        else callback(null);
+        // If the response is incorrect, try to redirect.
+        else redirect();
       }, function(response) {
-        callback(null);
+        // If there was an error, try to redirect.
+        redirect();
       });
     }
+  }
+
+  this.getToken = function(callback) {
+    if($rootScope.oauthToken) callback($rootScope.oauthToken);
+    else getTokenHelper(function(token) {
+      $rootScope.oauthToken = token;
+      callback($rootScope.oauthToken);
+    });
   };
 }]);
