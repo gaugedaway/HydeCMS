ef76dbb4f8ed37939fde9058c563ed5bdec9e0bd
Adam Bac <abac00s@users.noreply.github.com>
2016-03-20 17:03:31 +0000

    Changed application entry point

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  entry: './src/main.jsx',
+  entry: './src/app/index.jsx',
   output: { path: __dirname, filename: 'bundle.js' },
   module: {
     loaders: [
