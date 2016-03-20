7622aa53fe20c9b7cd51899db89011da1d38fc18
Adam Bac <abac00s@users.noreply.github.com>
2016-03-20 17:22:48 +0000

    Exported action types to constants

--- src/app/actions/authentication.js
+++ src/app/actions/authentication.js
@@ -1,22 +1,28 @@
 import fetch from 'isomorphic-fetch'
 import { authenticationUrl } from '../consts.js'
 
+// actions types
+
+export const REQUEST_AUTHENTICATION = 'REQUEST_AUTHENTICATION'
+export const AUTHENTICATE_SUCCESS = 'AUTHENTICATE_SUCCESS'
+export const AUTHENTICATE_ERROR = 'AUTHENTICATE_ERROR'
+
 export function requestAuthentication() {
   return {
-    type: 'REQUEST_AUTHENTICATION'
+    type: REQUEST_AUTHENTICATION
   }
 }
 
 export function authenticationSuccess(token) {
   return {
-    type: 'AUTHENTICATE_SUCCESS',
+    type: AUTHENTICATE_SUCCESS,
     token
   }
 }
 
 export function authenticationError(error) {
   return {
-    type: 'AUTHENTICATE_ERROR',
+    type: AUTHENTICATE_ERROR,
     error
   }
 }
