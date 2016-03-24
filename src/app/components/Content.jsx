7f89b91da50b47b37b9ac7a6e9d7d3dbb653bb7b
Adam Bac <abac00s@users.noreply.github.com>
2016-03-24 10:12:37 +0000

    Fixed typo

--- src/app/components/Content.jsx
+++ src/app/components/Content.jsx
@@ -4,10 +4,10 @@ import React from 'react'
 export default function Content({ page }) {
   switch(page) {
     case 'main':
-      return <h1>This is the main page's content</h1>
+      return <h1>This is the main page content</h1>
       
     case 'second':
-      return <h1>This is the second page's content</h1>
+      return <h1>This is the second page content</h1>
       
     default:
       return <h1>Error: page not found!</h1>
