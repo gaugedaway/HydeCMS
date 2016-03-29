05c7661a39b2e2519178ae276f7079751ae1c008
Adam Bac <abac00s@users.noreply.github.com>
2016-03-29 19:01:03 +0000

    Added logout button

--- src/app/components/Dashboard.jsx
+++ src/app/components/Dashboard.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default class Dashboard extends React.Component {
   render() {
-    return <h1>You have successfully logged in!</h1>
+    return (
+      <div>
+        <h1>You have successfully logged in!</h1>
+        <button onClick={ this.props.logout }>Logout</button>
+      </div>
+    )
   }
 }
--- src/app/containers/DashboardContainer.js
+++ src/app/containers/DashboardContainer.js
@@ -1,5 +1,6 @@
 import { connect } from 'react-redux'
 
+import { logout } from '../actions/token.js'
 import Dashboard from '../components/Dashboard.jsx'
 
 function mapStateToProps(state, ownProps) {
@@ -8,6 +9,12 @@ function mapStateToProps(state, ownProps) {
   }
 }
 
-const DashboardContainer = connect(mapStateToProps)(Dashboard)
+function mapDispatchToProps(dispatch, ownProps) {
+  return {
+    logout: () => dispatch(logout())
+  }
+}
+
+const DashboardContainer = connect(mapStateToProps, mapDispatchToProps)(Dashboard)
 
 export default DashboardContainer
