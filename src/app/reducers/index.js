065a8328a853b21900007a55166b13f59c958f60
Adam Bac <abac00s@users.noreply.github.com>
2016-03-29 19:09:13 +0000

    Split reducer into separate files

--- src/app/reducers/index.js
+++ src/app/reducers/index.js
@@ -1,57 +1,8 @@
 import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux'
 
-import {
-  REQUEST_TOKEN,
-  REQUEST_TOKEN_SUCCESS,
-  REQUEST_TOKEN_ERROR,
-  REQUEST_TOKEN_CANCEL,
-  RESET_TOKEN
-} from '../actions/token.js'
+import tokenReducer from './tokenReducer.js'
 
-function tokenReducer(token = { value: null, requested: false, error: null }, action) {
-  switch (action.type) {
-    case REQUEST_TOKEN:
-      return {
-        value: null,
-        requested: true,
-        error: null
-      }
-      
-    case REQUEST_TOKEN_SUCCESS:
-      return {
-        value: action.payload,
-        requested: false,
-        error: null
-      }
-      
-    case REQUEST_TOKEN_ERROR:
-      return {
-        value: null,
-        requested: false,
-        error: action.error
-      }
-      
-    case REQUEST_TOKEN_CANCEL:
-      return {
-        value: token.value,
-        requested: false,
-        error: token.error
-      }
-      
-    case RESET_TOKEN:
-      return {
-        value: null,
-        requested: false,
-        error: null
-      }
-    
-    
-    
-    default:
-      return token
-  }
-}
 
 const mainReducer = combineReducers({
   token: tokenReducer,
