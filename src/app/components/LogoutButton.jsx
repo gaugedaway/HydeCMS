688f7d25588ebc9471464c2d33126b446b389c2e
Adam Bac <abac00s@users.noreply.github.com>
2016-03-31 09:33:20 +0000

    Added LogoutButton

---
+++ src/app/components/LogoutButton.jsx
@@ -0,0 +1,31 @@
+/* global localStorage */
+
+import React from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { push } from 'react-router-redux'
+
+import { LOCAL_STORAGE_TOKEN_KEY } from '../config.js'
+import { resetToken } from '../actions/token.js'
+
+class LogoutButton extends React.Component {
+  logout() {
+    localStorage[LOCAL_STORAGE_TOKEN_KEY] = JSON.stringify(null)
+    this.props.resetToken()
+    this.props.push({ pathname: '/logout' })
+  }
+  
+  render() {
+    return <button onClick={ this.logout.bind(this) }>Logout</button>
+  }
+}
+
+function mapStateToProps() {
+  return {}
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ resetToken, push }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(LogoutButton)
