690dd1d6704e6dc9d6ecdbf1b6335f37e945df60
Adam Bac <abac00s@users.noreply.github.com>
2016-03-31 09:35:47 +0000

    Removed Container part from Dashboard

--- src/app/components/Dashboard.jsx
+++ src/app/components/Dashboard.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
+import LogoutButton from './LogoutButton.jsx'
+
 export default class Dashboard extends React.Component {
   render() {
     return (
       <div>
         <h1>You have successfully logged in!</h1>
-        <button onClick={ this.props.logout }>Logout</button>
+        <LogoutButton />
       </div>
     )
   }
