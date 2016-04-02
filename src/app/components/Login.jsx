07513630240f160d4e226f197aa39556c162fcba
Adam Bac <abac00s@users.noreply.github.com>
2016-04-02 20:19:16 +0000

    Simplified the code with some regex

--- src/app/components/Login.jsx
+++ src/app/components/Login.jsx
@@ -8,22 +8,15 @@ import * as AccountActions from '../actions/account.js'
 
 class Login extends React.Component {
   getCode() {
-    const code = window.location.href.match(/\?code=(.*)/)
-    if(!code) return null
-    return code[1].split('#')[0]
+    const code = window.location.href.match(/\?code=(\w*)/)
+    if(code) return code[1]
   }
-  
+
   cleanUrl() {
-    const location = window.location
-    let clean_uri = location.protocol + "//" + location.host + location.pathname
-    const hash_pos = location.href.indexOf("#")
-    if (hash_pos > 0) {
-        const hash = location.href.substring(hash_pos, location.href.length)
-        clean_uri += hash
-    }
+    let clean_uri = window.location.href.replace(/\?code=\w*/, '')
     window.history.replaceState({}, document.title, clean_uri)
   }
-  
+
   componentDidMount() {
     const code = this.getCode()
     if(code) {
@@ -31,15 +24,15 @@ class Login extends React.Component {
       this.props.fetchAccount(code, { pathname: '/' })
     }
   }
-  
+
   componentWillUnmount() {
     this.props.fetchAccountCancel()
   }
-  
+
   redirectToGithubAuth() {
     window.location.href = `${ OAUTH_URL }?redirect_uri=${ REDIRECT_URI }&client_id=${ CLIENT_ID }&scope=${ SCOPE.join(',') }`
   }
-   
+
   render() {
     return (
       <div>
